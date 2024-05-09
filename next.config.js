246900465d63a35d4855e52f9d3d5a61e6ce5b63
Renan Cardoso <renancardoso0307@gmail.com>
2024-05-09 09:41:54 -0300

    chore: new config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",  // <=== enables static exports
-    reactStrictMode: true,
+    distDir: 'dist',
   };
   
   module.exports = nextConfig;
